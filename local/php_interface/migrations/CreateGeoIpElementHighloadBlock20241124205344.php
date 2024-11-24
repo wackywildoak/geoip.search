0468abe2483f88c4b9772a78da533beb57deb0ce
wackywildoak <wackywildoak@gmail.com>
2024-11-25 03:27:38 +0900

    feat(migration): create geoiphighload element mirgration

---
+++ local/php_interface/migrations/CreateGeoIpElementHighloadBlock20241124205344.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class CreateGeoIpElementHighloadBlock20241124205344 extends Version
+{
+    protected $author = "admin";
+
+    protected $description   = "";
+
+    protected $moduleVersion = "4.15.1";
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $this->getExchangeManager()
+             ->HlblockElementsImport()
+             ->setExchangeResource('hlblock_elements.xml')
+             ->setLimit(20)
+             ->execute(function ($item) {
+                 $this->getHelperManager()
+                      ->Hlblock()
+                      ->addElement(
+                          $item['hlblock_id'],
+                          $item['fields']
+                      );
+             });
+    }
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function down()
+    {
+    }
+
+
+}
